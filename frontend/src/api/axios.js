a4258a3a790ebc7573fd558a4653c374b4df18b0
sahasshinde <sahasshinde3@gmail.com>
2026-03-19 18:06:51 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api',
+  baseURL: ' https://zwigato-backend-h8y2.onrender.com/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
